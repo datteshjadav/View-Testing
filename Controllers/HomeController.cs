9e110d59a9e1c900f5cb0ac9c6b23d8e64edb54d
kodwaniishika <ishikakodwani717@gmail.com>
2024-03-11 17:49:49 +0530

    Tabassetview

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,6 +17,9 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult TabAsset(){
+        return View();
+    }
 
     public IActionResult Privacy()
     {
