b179fee3a38497f9ff5103a4f17e420e06dc10b0
Dev1606 <devmevawala96@gmail.com>
2024-03-11 15:36:52 +0530

    TabAudit View Done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,6 +17,11 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult TabAudit()
+    {
+        return View();
+    }
 
     public IActionResult Privacy()
     {
